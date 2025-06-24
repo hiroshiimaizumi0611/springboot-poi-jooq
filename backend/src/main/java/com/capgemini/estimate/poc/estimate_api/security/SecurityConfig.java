511b58c0fe120b589797da7f7083b6d92a258f26
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2025-06-24 11:34:01 +0900

    backend v2

--- backend/src/main/java/com/capgemini/estimate/poc/estimate_api/security/SecurityConfig.java
+++ backend/src/main/java/com/capgemini/estimate/poc/estimate_api/security/SecurityConfig.java
@@ -40,7 +40,6 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("*"));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
     configuration.setAllowedHeaders(Arrays.asList("*"));
     configuration.setAllowCredentials(true);
