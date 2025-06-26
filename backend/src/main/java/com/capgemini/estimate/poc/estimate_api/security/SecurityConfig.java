e231e2ca1f2a74af2405061a5fee025d0c61d3f1
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2025-06-26 10:57:05 +0900

    commnet out

--- backend/src/main/java/com/capgemini/estimate/poc/estimate_api/security/SecurityConfig.java
+++ backend/src/main/java/com/capgemini/estimate/poc/estimate_api/security/SecurityConfig.java
@@ -1,20 +1,15 @@
 package com.capgemini.estimate.poc.estimate_api.security;
 
 import com.capgemini.estimate.poc.estimate_api.auth.JwtAuthenticationFilter;
-import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
