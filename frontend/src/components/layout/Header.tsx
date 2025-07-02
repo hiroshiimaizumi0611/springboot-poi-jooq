9a07a085e8f2cd8bb080cb54a647a2f63df39cab
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2025-07-02 16:06:56 +0900

    header fix

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -8,7 +8,7 @@ const logoutFunc = async () => {
 
 export const Header = () => (
   <header className="h-16 bg-blue-500 border-b flex items-center justify-between px-6">
-    <div className="font-bold text-xl">見積WEB</div>
+    <div className="font-bold text-xl">見積君</div>
     <div className="flex items-center gap-4">
       <span>123456 営業部</span>
       <span>〇〇太郎</span>
